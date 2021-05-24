7f279dcb18c3dbf21466fa2dc68b08cab9b7bd48
umang1s <umpsiit95@gmail.com>
2021-05-24 15:19:52 +0530

    fourth

--- main.c
+++ main.c
@@ -15,22 +15,68 @@ Team 14
     */
 
 #include<stdio.h>
-#include"slr.h"
-#include"file.h"
+#define input "Input.txt"
+#define output "Output.txt"
+
+/***************************************variable declartion**********************************/
+char non_terminal[100];
+int count_non_terminal=0;
+
+char terminal[200];
+int count_terminal=0;
+
+int no_of_production=0;
 
 
+/***************************************function declartion*********************************/
+
+void readInput();
+void writeOutput();
+void printOutPut();
+void makeCanonical();
+void solveSlr();
 
 
+/****************************************Main File*****************************************/
 int main(){
+
     printf("---------------------------------------------------------------\n");
     printf("------------------------SLR Parser-----------------------------\n");
-    printf("---------------------------------------------------------------\n");
+    printf("---------------------------------------------------------------\n\n");
 
-    getInputFromFile();
+    printf("reading input...\t\t");
+    readInput();
+    printf("completed\n");
+    
 
 
-    printOutput();
-
-    writeOutPutInFile();
 
 }
+
+/*********************************function defination*************************************/
+void readInput(){ //for reading input from file
+    FILE *f = fopen(input, "r");
+    int c = getc(f);
+    while (c != EOF) {
+        putchar(c);
+        c = getc(f);
+    }
+}
+
+void writeOutput(){//for writing output in file
+
+}
+
+void printOutput(){ //for displaying output in monitor
+
+}
+
+void solveSlr(){
+
+}
+
+void makeCanonical(){
+
+}
+
+
