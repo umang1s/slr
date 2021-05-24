46a76bc6b5cac2a15626949b269cc30dcc1a6bf5
umang1s <umpsiit95@gmail.com>
2021-05-24 14:35:37 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+Team 14
+    Name    :   Umang Maurya
+    Roll no :   055
+
+    Name    :   Amardeep Singh(Lata)
+    Roll no :   009
+
+    Name    :   Yateesh Chandra
+    Roll no :   057
+
+    Name    :   Sachin Verma
+    Roll no :   038
+
+    */
+
+#include<stdio.h>
+#include<string.h>
+#include"slr.h"
+#include"file.h"
+
+
+int main(){
+    printf("---------------------------------------------------------------\n");
+    printf("------------------------SLR Parser-----------------------------\n");
+    printf("---------------------------------------------------------------\n");
+
+
+}
